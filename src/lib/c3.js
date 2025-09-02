297bb15e1d4aac5b28aa7afec117e8938f86584d
albanovs <albvnovs@gmail.com>
2025-09-02 14:56:18 +0600

    c3 added

---
+++ src/lib/c3.js
@@ -0,0 +1,23 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+export const uploadFile = async (fileBuffer, fileType) => {
+  const fileKey = `${uuidv4()}.${fileType.split("/")[1]}`;
+  const params = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: fileKey,
+    Body: fileBuffer,
+    ContentType: fileType,
+    ACL: "public-read",
+  };
+  await s3.send(new PutObjectCommand(params));
+  return `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`;
+};
